3393241ca40f91d45bd0c254cf064c05dcaea340
Reavids101 <134131470+Reavids101@users.noreply.github.com>
2023-07-12 14:33:07 +0800

    Add files via upload

--- article-home.js
+++ article-home.js
@@ -2,73 +2,92 @@
 var articles = [];
 
 // Fetch the JSON files and add the articles to the array
-for (var i = 0; i < 3; i++) {
-  var url = "https://raw.githubusercontent.com/reavids101/risk-engineering-aon/main/article (" + i + ").json";
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      articles = articles.concat(data);
-      
-      // Get the articles container element
-      var articlesContainer = document.getElementById("articles");
+fetch("./")
+  .then(response => response.text())
+  .then(data => {
+    // Parse the HTML response into a DOM tree
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(data, "text/html");
 
-      // Loop through each article and create an article preview
-      for (var i = 0; i < articles.length; i++) {
-        var article = articles[i];
+    // Get the list of files in the directory
+    var fileList = Array.from(htmlDoc.querySelectorAll("a")).map(a => a.href);
 
-        // Create a div element for the article preview
-        var articlePreview = document.createElement("div");
-        articlePreview.classList.add("article-preview");
+    // Filter the list to only include the JSON files
+    var jsonFiles = fileList.filter(file => file.endsWith(".json"));
 
-        // Create the article preview thumbnail element
-        var thumbnail = document.createElement("img");
-        thumbnail.classList.add("thumbnail");
-        thumbnail.src = article.thumbnailURL;
-        thumbnail.addEventListener("click", function() {
-          // Open the article in a new browser tab
-          window.open(article.browserURL);
-        });
-        articlePreview.appendChild(thumbnail);
+    // Use the JSON files to generate the filenames array
+    var filenames = jsonFiles.map(file => file.substring(file.lastIndexOf("/") + 1));
 
-        // Create the article preview title element
-        var title = document.createElement("h2");
-        title.textContent = article.title;
-        articlePreview.appendChild(title);
+    // Loop through each JSON file and add its articles to the array
+    for (var i = 0; i < filenames.length; i++) {
+      var url = filenames[i];
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          articles = articles.concat(data);
+          
+          // Get the articles container element
+          var articlesContainer = document.getElementById("articles");
 
-        // Create the article preview author element
-        var author = document.createElement("div");
-        author.classList.add("author");
-        author.textContent = "By " + article.author;
-        articlePreview.appendChild(author);
+          // Loop through each article and create an article preview
+          for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
 
-        // Create the article preview snippet element
-        var snippet = document.createElement("div");
-        snippet.classList.add("snippet");
-        snippet.textContent = article.snippet;
-        articlePreview.appendChild(snippet);
+            // Create a div element for the article preview
+            var articlePreview = document.createElement("div");
+            articlePreview.classList.add("article-preview");
 
-        // Create the view button element
-        var viewBtn = document.createElement("button");
-        viewBtn.classList.add("view-btn");
-        viewBtn.textContent = "View PDF";
-        viewBtn.addEventListener("click", function() {
-          // Remove any existing PDF viewer element
-          var pdfViewerContainer = document.getElementById("pdf-viewer");
-          while (pdfViewerContainer.firstChild) {
-            pdfViewerContainer.removeChild(pdfViewerContainer.firstChild);
+            // Create the article preview thumbnail element
+            var thumbnail = document.createElement("img");
+            thumbnail.classList.add("thumbnail");
+            thumbnail.src = article.thumbnailURL;
+            thumbnail.addEventListener("click", function() {
+              // Open the article in a new browser tab
+              window.open(article.browserURL);
+            });
+            articlePreview.appendChild(thumbnail);
+
+            // Create the article preview title element
+            var title = document.createElement("h2");
+            title.textContent = article.title;
+            articlePreview.appendChild(title);
+
+            // Create the article preview author element
+            var author = document.createElement("div");
+            author.classList.add("author");
+            author.textContent = "By " + article.author;
+            articlePreview.appendChild(author);
+
+            // Create the article preview snippet element
+            var snippet = document.createElement("div");
+            snippet.classList.add("snippet");
+            snippet.textContent = article.snippet;
+            articlePreview.appendChild(snippet);
+
+            // Create the view button element
+            var viewBtn = document.createElement("button");
+            viewBtn.classList.add("view-btn");
+            viewBtn.textContent = "View PDF";
+            viewBtn.addEventListener("click", function() {
+              // Remove any existing PDF viewer element
+              var pdfViewerContainer = document.getElementById("pdf-viewer");
+              while (pdfViewerContainer.firstChild) {
+                pdfViewerContainer.removeChild(pdfViewerContainer.firstChild);
+              }
+
+              // Create a new PDF viewer element
+              var pdfViewer = document.createElement("iframe");
+              pdfViewer.src = article.pdfURL;
+              pdfViewer.classList.add("pdf-viewer");
+              pdfViewerContainer.appendChild(pdfViewer);
+            });
+            articlePreview.appendChild(viewBtn);
+
+            // Add the article preview to the articles container
+            articlesContainer.appendChild(articlePreview);
           }
-
-          // Create a new PDF viewer element
-          var pdfViewer = document.createElement("iframe");
-          pdfViewer.src = article.pdfURL;
-          pdfViewer.classList.add("pdf-viewer");
-          pdfViewerContainer.appendChild(pdfViewer);
-        });
-        articlePreview.appendChild(viewBtn);
-
-        // Add the article preview to the articles container
-        articlesContainer.appendChild(articlePreview);
-      }
-    })
-    .catch(error => console.error("Error loading articles:", error));
-}
+        })
+        .catch(error => console.error("Error loading articles:", error));
+    }
+  })
+  .catch(error => console.error("Error getting file list:", error));
