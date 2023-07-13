b8c68010f888825bd2d23c79d4ed75f5d9f23857
Reavids101 <134131470+Reavids101@users.noreply.github.com>
2023-07-13 09:00:10 +0800

    Add files via upload

--- article-home.js
+++ article-home.js
@@ -5,7 +5,7 @@ const baseURL = "https://raw.githubusercontent.com/reavids101/risk-engineering-a
 const fileNames = [
   "Wellington Letter September 7, 2020.json",
   "Steam Explosions.json",
-  "US CSB 2020 Hurricane Season_Guidance for Chemical Plants During Extreme Weather Events.json",
+  "US CSB 2020 Hurricane Season_Guidance for Chemical Plants During Extreme Weather Events.json"
   // add more file names as needed
 ];
 
@@ -33,9 +33,9 @@ function createArticleElement(article) {
   element.classList.add("article");
   element.innerHTML = `
     <h2 class="article-title">${article.title}</h2>
-    <p class="article-date">${article.date}</p>
-    <p class="article-summary">${article.summary}</p>
-    <a class="article-read-more" href="${article.url}">Read more</a>
+    <p class="article-author">${article.author}</p>
+    <p class="article-snippet">${article.snippet}</p>
+    <a class="article-read-more" href="${article.pdfUrl}">Read more</a>
   `;
   return element;
 }
