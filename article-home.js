ee3289d9a5e7c7f2d829e483b4fccd14b9b05cce
Reavids101 <134131470+Reavids101@users.noreply.github.com>
2023-07-13 10:44:23 +0800

    Add files via upload

--- article-home.js
+++ article-home.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  
 // Define a variable that holds the base URL
 const baseURL = "https://raw.githubusercontent.com/reavids101/risk-engineering-aon/main/";
 
@@ -5,7 +7,22 @@ const baseURL = "https://raw.githubusercontent.com/reavids101/risk-engineering-a
 const fileNames = [
   "Wellington Letter September 7, 2020.json",
   "Steam Explosions.json",
-  "US CSB 2020 Hurricane Season_Guidance for Chemical Plants During Extreme Weather Events.json"
+  "US CSB 2020 Hurricane Season_Guidance for Chemical Plants During Extreme Weather Events.json",
+  "AIM Learning Series 2.0.json",
+  "alternative-heating.json",
+  "Aon Hurricane Season Preparation and Response 2020.json",
+  "NFPA 70B Checklist NATURAL DISASTER ELECTRICAL EQUIPMENT.json",
+  "Management Of Change MOC.json",
+  "icheme-lessons-learned-database.json",
+  "IBHS-Small Hail-Big Problems-New Approach 20230419.json",
+  "Hazards of High Oxygen Concentration_English.json",
+  "Get Help.lnk.json",
+  "Freezing-Weather-Last-Minute-Checklist_IBHS.json",
+  "Freezing-Bursting-Pipes_IBHS.json",
+  "FreezeTips.json",
+  "Combustible Mists_English.json",
+  "Automatic vs Manual control.json",
+  "202201beaconenglish-PID.json"
   // add more file names as needed
 ];
 
@@ -20,6 +37,10 @@ Promise.all(fileNames.map((fileName) => fetch(`${baseURL}${fileName}`).then((res
 
 function renderArticles(articles) {
   const container = document.getElementById("articles");
+  if (!container) {
+    console.error("Error: Cannot find container element with ID 'articles'");
+    return;
+  }
   container.innerHTML = "";
 
   articles.forEach((article) => {
@@ -33,9 +54,9 @@ function createArticleElement(article) {
   element.classList.add("article");
   element.innerHTML = `
     <h2 class="article-title">${article.title}</h2>
-    <p class="article-author">${article.author}</p>
     <p class="article-snippet">${article.snippet}</p>
     <a class="article-read-more" href="${article.pdfUrl}">Read more</a>
   `;
   return element;
 }
+});
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+document.addEventListener('DOMContentLoad', function() {
+  
 const element = document.querySelector('.gallery-item');
-element.classList.add('animate-slide-up');
+console.log(element);
+if (element) {
+  element.classList.add('animate-slide-up');
+}
 
 // Get necessary elements
 const gallery = document.querySelector('.gallery');
@@ -94,97 +99,51 @@ function generatePaginationCircles() {
 generatePaginationCircles();
 updatePagination();
 
+});
 
+function filterArticles() {
+  const container = document.getElementById("articles");
+  const input = document.getElementById("searchBox").value.toUpperCase();
+  const articles = container.getElementsByClassName("article-preview");
+  const suggestions = document.getElementById("suggestions");
 
+  // Loop through all the article preview elements
+  for (let i = 0; i < articles.length; i++) {
+    const article = articles[i];
+    const searchString = article.getAttribute("data-search").toUpperCase();
 
-function loadYouTubeAPI() {
-    if (typeof YT === 'undefined' || typeof YT.Player === 'undefined') {
-        var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api?api_key=AIzaSyB8PZzheb2PUkyrFwBdDA4SoYGRS8p8kLQ";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
-}
+    // Calculate the relevance score of the article based on the search term
+    const relevanceScore = searchString.split(input).length - 1;
+    console.log(`Article ${i}: relevance score = ${relevanceScore}, search term = ${input}`);
 
-function onYoutubeIframeAPIReady() {
-    new YT.Player('youtube-player', {
-        width: '640',
-        height: '390',
-        videoId: 'VIDEO_ID',
-        playerVars: {
-            autoplay: 1,
-            controls: 0,
-            showinfo: 0,
-            modestbranding: 1,
-            loop: 1
-        },
-        events: {
-            onReady: onPlayerReady
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    event.target.mute();
-    event.target.playVideo();
-}
-
-window.onload = loadYouTubeAPI
-
-async function fetchMultipleVideoDetails(videoIds) {
-
-    try {
-        const videoDetails=[];
-
-        for (const videoId of videoIds) {
-            const response = await fetch ('https://graph.microsoft.com/v1.0/me/drive/items${videoId}', {
-                headers: {
-                    'Authorization': 'Bearer ${accessToken}'
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error('Error fetching video details for video ID: ${videoId}');
-            }
-            const data = await response.json();
-            const videoName = data.name;
-            const videoThumbnailUrl = data.thumbnailUrl;
-
-            videoDetails.push({ videoId, videoName, videoThumbnailUrl });
-        }
-        return videoDetails;
-    } catch (error) {
-        console.error('Error:', error);
-        return [];
-    }
-}
-
-async function displayVideoThumbnails() {
-    try {
-    const response = await fetch('https://graph.microsoft.com/v1.0/me/drive/items/{folder-id}/children?$filter=video', {
-        headers: {
-            'Authorization': 'Bearer ${accessToken)'
-        }
-    });
-
-    if (!response.ok) {
-        throw new Error('Error fetchingvideo IDs');
+    // If the search string is found, show the article preview, otherwise hide it
+    if (relevanceScore > 0) {
+      article.style.display = "";
+    } else {
+      article.style.display = "none";
     }
 
-    const data = await response.json();
-    const videoIds = data.value.map(video => video.id);
-    const videoDetails= await fetchMultipleVideoDetails(videoIds);
-    const thumbnailContainer = document.getElementById('thumbnail-container');
-    videoDetails.forEach(video => {
-        const thumbnailElement = document.createElement('div');
-        thumbnailElement.classList.add('thumbnail');
-        thumbnailElement.innerHTML = '<img src="$(video.videoThumbnailUrl}" alt="${video.videoName}"> <p>${video.videoName}</p>';
-        thumbnailContainer.appendChild(thumbnailElement);
-    });   
+    // Set the data-relevance attribute of the article to its relevance score
+    article.setAttribute("data-relevance", relevanceScore);
+  }
 
-} catch (error) {
-    console.error('Error:', error);
-}
-}
+  // Sort the article preview elements based on their relevance score
+  const sortedArticles = Array.from(articles).sort((a, b) => {
+    const aRelevance = parseInt(a.getAttribute("data-relevance"));
+    const bRelevance = parseInt(b.getAttribute("data-relevance"));
+    return bRelevance - aRelevance;
+  });
 
-displayVideoThumbnails();
+  // Append the sorted article preview elements to the container
+  sortedArticles.forEach(article => container.appendChild(article));
+
+  // Generate a list of suggestions based on the available search terms
+  const searchTerms = container.getAttribute("data-search").split(" ");
+  const matchingTerms = searchTerms.filter(term => term.toUpperCase().indexOf(input) > -1);
+  suggestions.innerHTML = "";
+  matchingTerms.forEach(term => {
+    const option = document.createElement("option");
+    option.value = term;
+    suggestions.appendChild(option);
+  });
+}
