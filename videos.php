5f8e5d08bdf7bd08072b8562e6b0d76ea765c149
Reavids101 <134131470+Reavids101@users.noreply.github.com>
2023-07-18 11:19:52 +0800

    Add files via upload

--- videos.php
+++ videos.php
@@ -1,4 +1,4 @@
-<?php
+
 $dir = '/risk-engineering-aon/'; // Directory containing the raw video files
 $videos = array();
 
@@ -31,4 +31,3 @@ if ($handle = opendir($dir)) {
 
 // Output the video information as JSON
 echo json_encode($videos);
-?>
